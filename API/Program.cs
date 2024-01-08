6eed102b96d2e4efcd581a741e16c9580f01e1c2
iamantoon <oneloveeeeeeant@gmail.com>
2024-01-08 23:49:54 +0200

    API start

---
+++ API/Program.cs
@@ -0,0 +1,35 @@
+// This creates an instance of this web application builder with some pre-configured defaults
+// When we use this command, it's going to configure what's referred to a Kestrel web server
+// That's responsible for actually running our application
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<StoreContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
