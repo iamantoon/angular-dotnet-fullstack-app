cd8bfbea4149723a9c7e0873e123d3823c071e2d
iamantoon <oneloveeeeeeant@gmail.com>
2024-01-18 22:33:26 +0200

    Client checkout

--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { DeliveryMethod } from '../shared/models/deliveryMethod';
+import { map } from 'rxjs';
+import { Order, OrderToCreate } from '../shared/models/order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getDeliveryMethod(){
+    return this.http.get<DeliveryMethod[]>(this.baseUrl + 'orders/deliveryMethods').pipe(
+      map(dm => {
+        return dm.sort((a, b) => b.price - a.price)
+      })
+    )
+  }
+
+  createOrder(order: OrderToCreate){
+    return this.http.post<Order>(this.baseUrl + 'orders', order);
+  }
 }
